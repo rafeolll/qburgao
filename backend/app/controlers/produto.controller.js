9fecf61bb801ad0e82b58a34ea07ed6f610558f7
rafaeolll <rafael.y.paul@gmail.com>
2023-05-31 14:54:48 -0300

    Projeto Lanchonnete - Qburgao

--- backend/app/controlers/produto.controller.js
+++ backend/app/controlers/produto.controller.js
@@ -1,6 +1,28 @@
 const ProdutoModel = require("../models/produtomodel");
 
-exports.create = (req, res) =>  {}
+exports.create = (req, res) =>  {
+    if (!req.body.nome || !req.body.valor){
+        res.status(400).send({
+            message:" conteudo do corpo da requisição vazia"
+        });
+    } else {
+        const produto = new ProdutoModel({
+            nome: req.body.nome,
+            valor: req.body.valor
+        });
+
+        ProdutoModel.create(produto, (err,data) => {
+            if (err){
+                res.status(500).send({
+                    message: err.message || "Ocorrreu um erro ao inserir os dados"
+                });
+            } else{
+                res.send(data);
+            }
+        })
+    }
+
+}
 exports.findAll = (req, res) =>  {
     ProdutoModel.getAll((err, data) => 
     {
@@ -14,7 +36,23 @@ exports.findAll = (req, res) =>  {
 
     })
 }
-exports.findById = (req, res) =>  {}
+exports.findById = (req, res) =>  {
+    ProdutoModel.findById(req.params.produtoId, (err,data) => {
+        if (err){
+            if(err.type =="not_found"){
+                res.status(404).send({
+                    message: "Produto Não encontrado. ID"+req.params.produtoId
+                });
+            } else{
+                res.status(500).send({
+                    message: "Erro ao retornar o produto com ID: "+req.params.produtoId
+                });
+            }
+        }else {
+            res.send(data);
+        }
+    })
+}
 exports.update = (req, res) =>  {}
 exports.delete = (req, res) =>  {}
 exports.deleteAll= (req, res) =>  {}
--- backend/app/models/produtomodel.js
+++ backend/app/models/produtomodel.js
@@ -1,16 +1,41 @@
 const sql = require("./db.js");
  
 const ProdutoModel = function(produto){
-    this.name = produto.nome;
+    this.nome = produto.nome;
     this.valor = produto.valor;
 }
 
 ProdutoModel.create = (produto, result) => {
+    sql.query("Insert into produtos set ?", produto, (err,res) => {
+        if (err){
+            console.log("Erro: ", err);
+            result(err,null);
+            return;
+        }
+        
+        console.log("Produto criado: ", {idprodutos: res.insertId, ...produto});
+        result(null, {idprodutos: res.insertId, ...produto});
+    })
 
 };
 
 ProdutoModel.findById = (produtoId, result) =>{
 
+    sql.query("Select * from produtos where idprodutos = "+produtoId, (err, res) => {
+     if (err) {
+        console.log("erro: ", err);
+        result (null, err);
+        return;
+     }
+     
+     if (res.length){
+        console.log("Produto Encontrado", res[0]);
+        result(null,res[0]);
+     } else {
+        result ({type: "not_found"}, null);
+        console.log("Produto não encontrado");
+     }
+    })
 };
 ProdutoModel.getAll = result => {
 
@@ -29,7 +54,19 @@ ProdutoModel.getAll = result => {
 };
 
 ProdutoModel.updateById = (produtoId, produto, result) => {
-
+    sql.query("UPDATE produtos SET nome = ?, valor = ? WHERE idprodutos = ?",
+        [produto.nome, produto.valor, produtoId], (err, res) => {
+            if (err){
+                console.log("erro: ", err);
+                result(null, err);
+            } else if (res.affectedRows == 0){
+                result({type: "not_found"}, null);
+            } else {
+                console.log("Produto atualizado: ", {idprodutos: produtoId, ...produto});
+                result(null, {idprodutos: produtoId, ...produto});
+            }
+        } 
+    )
 };
 
 ProdutoModel.remove = (produtoId, result) =>{
